a20b533bf2e02b9b900d9e943720a1bb8c2c16e6
JB <drv98@naver.com>
2024-10-22 15:00:56 +0900

    GUI 프로젝트2

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.swing.*;
+
+/**
+ * GUI 프로젝트 ver 1.0
+ */
+public class App {
+    public static void main(String[] args) {
+        new MainFrame("네이버 맵 검색");
+    }
+}
---
+++ src/main/java/org/example/MainFrame.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public JTextField addressTxt;  //주소 입력창
+    public JLabel resAddress, jibunAddress, resX, resY, imageLabel;
+
+    public MainFrame(String title) {
+        super(title);
+        //위쪽패널에 주소입력, 라벨, 버튼 추가
+        JPanel panel = new JPanel();
+        JLabel addressLbl = new JLabel("주소입력");
+        addressTxt = new JTextField(50);
+        JButton btn = new JButton("클릭");
+        panel.add(addressLbl);
+        panel.add(addressTxt);
+        panel.add(btn);
+        //가운데 이미지라벨
+        imageLabel = new JLabel("지도보기");
+        //아래쪽 패널
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new GridLayout(4, 1)); // 4줄
+        resAddress = new JLabel("도로명주소");
+        jibunAddress = new JLabel("지번주소");
+        resX = new JLabel("경도");
+        resY = new JLabel("위도");
+        panel2.add(resAddress);
+        panel2.add(jibunAddress);
+        panel2.add(resX);
+        panel2.add(resY);
+        
+        //레이아웃 설정
+        setLayout(new BorderLayout());
+        add(panel, BorderLayout.NORTH); //위의 패널을 윈도우창에 붙임.
+        add(imageLabel, BorderLayout.CENTER); //이미지라벨
+        add(panel2, BorderLayout.SOUTH); //패널2를 아래쪽에 붙임
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //윈도우 창 X 닫기
+        setSize(730, 660);                   //윈도우 창 크기
+        setVisible(true);                               //창 보이기
+    }
+}
